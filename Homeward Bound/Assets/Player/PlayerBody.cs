239a2c31cd8b1d79debc51f49d7ba4f8c517d59c
rak3me <rak3me@virginia.edu>
2019-01-26 02:29:45 -0500

    Wyatt's shitty movement is fixed

--- Homeward Bound/Assets/Player/PlayerBody.cs
+++ Homeward Bound/Assets/Player/PlayerBody.cs
@@ -6,29 +6,28 @@ public class PlayerBody : MonoBehaviour
 {
     Rigidbody rb;
     Vector3 smooth_speed;
-    float rot_speed = 500f, smooth_time = 0.1f;
+    float rot_speed = 500f, smooth_time = 0.2f;
 
     public float speed = 5f;
+
+	Vector3 dirGizmo;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    public void FixedUpdate()
+    public void Move(Vector3 dir, bool crouch)   //Moves player in the direction of dir at speed 
     {
-        if (rb.velocity != Vector3.zero)
-        {
-            transform.forward = Vector3.SmoothDamp(transform.forward, 
-                new Vector3(rb.velocity.x, transform.forward.y, rb.velocity.z), 
-                ref smooth_speed, smooth_time);
-        }
-    }
+		dirGizmo = dir;
+		if (rb.velocity != Vector3.zero) {
+			transform.forward = Vector3.SmoothDamp(transform.forward,
+			dir,
+			ref smooth_speed, smooth_time);
+			//transform.forward = Vector3.Lerp(transform.forward, dir, 100f * Time.deltaTime);
+		}
 
-
-    public void Move(Vector3 dir)   //Moves player in the direction of dir at speed 
-    {
-        rb.velocity = new Vector3(dir.x * speed, rb.velocity.y, dir.z * speed);
+		rb.velocity = new Vector3(dir.x * speed * (crouch ? 0.5f : 1), rb.velocity.y, dir.z * speed * (crouch ? 0.5f : 1));
     }
 
     public void Use()
