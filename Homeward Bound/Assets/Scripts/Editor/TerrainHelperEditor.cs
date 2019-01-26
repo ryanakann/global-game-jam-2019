7dc2e1fb460459aa28546476da262a1eb6347127
rak3me <rak3me@virginia.edu>
2019-01-26 13:00:42 -0500

    Terrain Helper (flatten, raise)

---
+++ Homeward Bound/Assets/Scripts/Editor/TerrainHelperEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TerrainHelper))]
+public class TerrainHelperEditor : Editor {
+
+	public override void OnInspectorGUI () {
+		base.OnInspectorGUI();
+
+		TerrainHelper targ = (TerrainHelper)target;
+
+		if (GUILayout.Button("Flatten Terrain")) {
+			targ.Flatten();
+		}
+	}
+}
---
+++ Homeward Bound/Assets/Scripts/TerrainHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainHelper : MonoBehaviour {
+	public float defaultHeight = 0f;
+
+	public void Flatten () {
+		Terrain terrain = GetComponent<Terrain>();
+		int width = terrain.terrainData.heightmapWidth;
+		int height = terrain.terrainData.heightmapHeight;
+		float[,] data = terrain.terrainData.GetHeights(0, 0, width, height);
+
+		float heightScale = terrain.terrainData.heightmapScale.y;
+		float adjustedHeight = Mathf.Clamp(defaultHeight, 0f, heightScale) / heightScale;
+
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				data[i, j] = adjustedHeight;
+			}
+		}
+		terrain.terrainData.SetHeights(0, 0, data);
+	}
+}
