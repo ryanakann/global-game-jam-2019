20f5a234cd45668752f596f385e4e1f7e0b4e9e7
Andrew-64 <31630789+Andrew-64@users.noreply.github.com>
2019-01-25 21:33:55 -0500

    PlayerBody

---
+++ Homeward Bound/Assets/PlayerBody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBody : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public void Move(Vector3 dir)   //Moves player in the direction of dir at speed 
+    {
+        dir.Normalize();
+        rb.velocity = dir * speed;
+        transform.rotation = Quaternion.LookRotation(dir);
+    }
+}
