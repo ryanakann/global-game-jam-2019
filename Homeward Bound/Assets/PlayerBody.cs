24985a1ba650bdad4599dc2fdd336adce3f6ef11
Wyatt Joyner <wcj3te@virginia.edu>
2019-01-25 22:37:37 -0500

    shitty code

--- Homeward Bound/Assets/PlayerBody.cs
+++ Homeward Bound/Assets/PlayerBody.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerBody : MonoBehaviour
 {
     Rigidbody rb;
+    Vector3 smooth_speed;
+    float rot_speed = 500f, smooth_time = 0.1f;
 
     public float speed;
 
@@ -16,9 +18,15 @@ public class PlayerBody : MonoBehaviour
 
     public void Move(Vector3 dir)   //Moves player in the direction of dir at speed 
     {
-        dir.Normalize();
-        rb.velocity = dir * speed;
-        transform.rotation = Quaternion.LookRotation(dir);
+
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, new Vector3(dir.x*speed, rb.velocity.y, dir.z*speed), ref smooth_speed, smooth_time);
+
+        if (rb.velocity != Vector3.zero)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, 
+                Quaternion.LookRotation(new Vector3(rb.velocity.x, 0, rb.velocity.z)), 
+                Time.deltaTime * rot_speed);
+        }
     }
 
     public void Use()
--- Homeward Bound/Assets/PlayerInput.cs
+++ Homeward Bound/Assets/PlayerInput.cs
@@ -5,24 +5,23 @@ public class PlayerInput : MonoBehaviour
 {
     PlayerBody body;
     Vector3 forward, dir;
+    float h, v;
     Camera cam;
 
     private void Start()
     {
         cam = Camera.main;
+        body = GetComponent<PlayerBody>();
     }
 
     private void Update()
     {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
-
-        if (dir.magnitude > 0f)
-        {
-            forward = Vector3.ProjectOnPlane(cam.transform.forward, Vector3.up);
-            dir = (v * forward + h * cam.transform.right).normalized;
-            body.Move(dir);
-        }
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        forward = Vector3.ProjectOnPlane(cam.transform.forward, Vector3.up);
+        dir = (v * forward + h * cam.transform.right);
+        dir = (dir.magnitude > 1f) ? dir.normalized : dir;
+        body.Move(dir);
 
         if (Input.GetAxis("Use") > 0f)
         {
