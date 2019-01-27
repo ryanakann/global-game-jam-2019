62df8980c77501afb5abec009663e109cf5d8c9c
Julian <jaykmcclinton@gmail.com>
2019-01-26 23:10:04 -0500

    passionate killer

---
+++ Homeward Bound/Assets/Scripts/CastleCrashers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CastleCrashers : MonoBehaviour
+{
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleCastle()
+    {
+        anim.SetTrigger("Toggle");
+    }
+}
