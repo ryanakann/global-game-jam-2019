410945e95baee1b09c34b0fdf9f46ff54953e393
Ryan McCampbell <rmccampbell7@gmail.com>
2019-01-27 17:37:33 -0500

    placeholder code for dragon quest completion

---
+++ Homeward Bound/Assets/DragonQuestComplete.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonQuestComplete : MonoBehaviour
+{
+    public void Complete()
+    {
+        //Nightbringer.instance.EndNight(Location.Cave);
+    }
+}
---
+++ Homeward Bound/Assets/TestDragonQuest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDragonQuest : MonoBehaviour
+{
+    public InventoryItem cat, money;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inventory.instance.AddItem(cat);
+        Inventory.instance.AddItem(money);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("has cat: " + Inventory.instance.HasItem(ItemID.Cat));
+        Debug.Log("has $$$: " + Inventory.instance.HasItem(ItemID.FiveBucks));
+    }
+}
