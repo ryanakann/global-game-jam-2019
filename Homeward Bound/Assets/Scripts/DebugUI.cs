ac21593f195df1a7dc857ba4e7144457eb3b140f
rak3me <rak3me@virginia.edu>
2019-01-27 01:42:15 -0500

    Post-Processing and debug

---
+++ Homeward Bound/Assets/Scripts/DebugUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugUI : MonoBehaviour {
+	public bool debugEnabled = true;
+
+	public Transform player;
+
+	public Transform oceanWaypoint;
+	public Transform mountainWaypoint;
+	public Transform forestWaypoint;
+
+	private void OnGUI () {
+		if (!debugEnabled) return;
+
+		if (GUILayout.Button("Ocean Teleport")) {
+			player.position = oceanWaypoint.position;
+		}
+		if (GUILayout.Button("Mountain Teleport")) {
+			player.position = mountainWaypoint.position;
+		}
+		if (GUILayout.Button("Forest Teleport")) {
+			player.position = forestWaypoint.position;
+		}
+	}
+}
