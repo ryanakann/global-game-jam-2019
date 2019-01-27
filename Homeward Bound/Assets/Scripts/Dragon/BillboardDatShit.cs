d7f4ff0af7850b7535f388c57f51121d241e1d9d
Simon Whittle <smw2ed@virginia.edu>
2019-01-27 03:56:00 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Homeward Bound/Assets/Scripts/Dragon/BillboardDatShit.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardDatShit : MonoBehaviour
+{
+    public Font font;
+
+    bool dewit = false;
+
+    /*
+    public Camera mainCamera;
+
+    private void Update() {
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.back,
+            mainCamera.transform.rotation * Vector3.up);
+    }
+    */
+
+
+    public void EVIL() {
+        PlayerInput.allowMovement = false;
+    }
+
+    public void MOREEEEVIL() {
+        PlayerInput.allowMovement = true;
+    }
+
+    public void ON() {
+        Debug.Log("ON");
+        dewit = true;
+    }
+
+    public void OFF() {
+        Debug.Log("OFF");
+        dewit = false;
+    }
+
+    private void OnGUI() {
+
+        if (dewit) {
+            GUIStyle style = new GUIStyle();
+            style.font = font;
+            style.fontSize = 18;
+            
+            // GUI.skin.label.font = font;
+
+            Vector2 worldPoint = Camera.main.WorldToScreenPoint(transform.position);
+            GUI.Label(
+                new Rect(worldPoint.x - 100, (Screen.height - worldPoint.y) - 50, 200, 100),
+                "Press E to Interact.", style
+                );
+        }
+    }
+
+}
