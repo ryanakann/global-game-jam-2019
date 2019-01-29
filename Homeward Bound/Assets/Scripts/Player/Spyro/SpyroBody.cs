a3d55ddf6d502dcc0294d1e9cd8fdf068099f989
Julian <jaykmcclinton@gmail.com>
2019-01-29 15:54:51 -0500

    it was incredible, youy shoud have seent it

---
+++ Homeward Bound/Assets/Scripts/Player/Spyro/SpyroBody.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpyroBody : MonoBehaviour {
+
+    Rigidbody rb;
+    Item equipped_item;
+    public Transform hand;
+
+    public Animator anim;
+    int speedHash, crouchHash, actionHash, actionIDHash;
+
+    [HideInInspector] public float speed = 20f;
+    float ground_dist = 0.1f;
+    Collider body_collider;
+    PlayerRagdoll ragdoll;
+
+    public bool moving;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        Inventory.instance.EquipEvent += Equip;
+        if (!anim)
+            anim = GetComponent<Animator>();
+        speedHash = Animator.StringToHash("Speed");
+        crouchHash = Animator.StringToHash("Crouching");
+        actionHash = Animator.StringToHash("Action");
+        actionIDHash = Animator.StringToHash("ActionID");
+        body_collider = GetComponent<Collider>();
+        DeathMachine.instance.DeathEvent += OnDeath;
+        SuccessMachine.instance.SuccessEvent += OnSuccess;
+        Respawner.instance.RespawnEvent += OnRespawn;
+        ragdoll = GetComponent<PlayerRagdoll>();
+    }
+
+    private void Update() {
+        if (!Physics.Raycast(transform.position,Vector3.down, 
+            out RaycastHit hit, ground_dist)) {
+            rb.drag = 0f;
+        } else {
+            rb.drag = 8f;
+        }
+    }
+
+    public void Use() {
+        equipped_item?.Use();
+    }
+
+    public void Interact() {
+
+    }
+
+    public void OpenInventory() {
+        Inventory.instance.Open();
+    }
+
+    public void Equip(InventoryItem i_item) {
+        if (equipped_item) {
+            Destroy(equipped_item.gameObject);
+        }
+        //spawn i_item.obj in hand, unless the i_item.id == ItemID.hand
+        if (i_item.id != ItemID.Hand) {
+            Item newItem = Instantiate(i_item.obj,hand).GetComponent<Item>();
+            //newItem.holder = this;
+            anim.SetInteger(actionIDHash,(int)newItem.id);
+        } else {
+            anim.SetInteger(actionIDHash,0);
+        }
+    }
+
+    public void OnDeath() {
+        ragdoll.ActivateRagdoll(true);
+        PlayerInput.allowMovement = false;
+    }
+
+    public void OnSuccess() {
+        PlayerInput.allowMovement = false;
+        Action(ActionID.Dance);
+    }
+
+    public void OnRespawn(Vector3 respawn_position) {
+        ragdoll.ActivateRagdoll(false);
+        PlayerInput.allowMovement = true;
+        transform.position = respawn_position;
+    }
+
+    public void Action(ActionID id) {
+        //anim.SetInteger(actionIDHash, (int)id);
+        anim.SetTrigger(actionHash);
+    }
+}
---
+++ Homeward Bound/Assets/Scripts/Player/Spyro/SpyroInput.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpyroInput : MonoBehaviour {
+    public GameObject headband;
+
+    SpyroBody body;
+    bool crouch;
+
+    public float InputX, InputZ;
+    public Vector3 desiredMoveDir;
+    public bool blockPlayerRot, forceGrounded;
+    public float desiredRotSpeed;
+    public float Speed;
+    public float allowPlayerRot;
+    public bool isGrounded;
+    private float verticalVel;
+
+    private Vector3 moveVector;
+    public Animator anim;
+    private CharacterController controller;
+
+    Camera cam;
+    public static bool allowMovement = true;
+
+    private void Start() {
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main;
+
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Use")) {
+            
+        }
+
+        if (Input.GetButtonDown("Headband")) {
+            headband.SetActive(!headband.activeSelf);
+
+        }
+
+        if (Input.GetButtonDown("Interact")) {
+            
+        }
+
+        if (Input.GetButtonDown("Inventory")) {
+            body.OpenInventory();
+        }
+
+        /*
+        if (Input.GetButtonDown("Ragdoll"))
+        {
+            //GetComponent<PlayerRagdoll>().ActivateRagdoll(true);
+            DeathMachine.instance.Kill(Vector3.zero);
+        }
+        */
+    }
+
+    private void FixedUpdate() {
+        InputMagnitude();
+
+        //Keep Character Grounded
+        if(forceGrounded){
+            if (isGrounded = controller.isGrounded) {
+                verticalVel -= 0;
+            } else {
+                verticalVel -= 2;
+            }
+
+            moveVector = new Vector3(0,verticalVel,0);
+            controller.Move(moveVector);
+        }
+    }
+
+    private void PlayerMoveAndRotation() {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        cam = Camera.main;
+        var forward = cam.transform.forward;
+        var right = cam.transform.right;
+
+        forward.y = 0f; right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        desiredMoveDir = forward * InputZ + right * InputX;
+        if (!blockPlayerRot) {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(desiredMoveDir),desiredRotSpeed);
+        }
+
+        controller.Move(desiredMoveDir);
+    }
+
+    public float damping = 0.0f; 
+    private void InputMagnitude() {
+        //Calc In vectors
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        anim.SetFloat("InputX", InputX, damping, 2f * Time.deltaTime);
+        anim.SetFloat("InputZ", InputZ, damping, 2f * Time.deltaTime);
+
+        //Calc Magnitude
+        Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+
+        //Physically move
+        anim.SetFloat("InputMagnitude", Speed, damping, Time.deltaTime);
+        if (Speed > allowPlayerRot)
+            PlayerMoveAndRotation();
+    }
+
+    private void OnDrawGizmos () {
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(transform.position, new Vector3(Input.GetAxis("Horizontal"), 
+            0f, Input.GetAxis("Vertical")) * 5f);
+    }
+}
